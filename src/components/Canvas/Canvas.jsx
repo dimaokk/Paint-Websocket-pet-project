0e3423ea048cf2bd56b421eb38878748efe7505f
dimaokk <dimadark216@gmail.com>
2022-05-12 20:30:44 +0700

    add brush

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import { observer } from "mobx-react-lite";
+import React, { useEffect, useRef } from "react";
+import canvasState from "../../store/canvasState";
+import toolState from "../../store/toolState";
+import Brush from "../tool/Brush";
 import styles from "./Canvas.module.scss";
 
-export const Canvas = () => {
+export const Canvas = observer(() => {
+  const ref = useRef();
+
+  useEffect(() => {
+    canvasState.setCanvas(ref.current);
+    toolState.setTool(new Brush(ref.current));
+  }, []);
   return (
     <div className={styles.canvas}>
-      <canvas width={900} height={500}></canvas>
+      <canvas ref={ref} width={900} height={500}></canvas>
     </div>
   );
-};
+});
--- src/components/ToolBar/ToolBar.jsx
+++ src/components/ToolBar/ToolBar.jsx
@@ -1,12 +1,46 @@
 import React from "react";
 import styles from "./ToolBar.module.scss";
-import { IoBrushOutline } from "react-icons/io5";
+import {
+  IoBrushOutline,
+  IoSquare,
+  IoEllipseOutline,
+  IoArrowBack,
+  IoArrowForward,
+  IoSaveOutline,
+} from "react-icons/io5";
+import { BsEraser, BsSlashLg } from "react-icons/bs";
 export const ToolBar = () => {
   return (
     <div className={styles.toolbar}>
-      <button className={styles.toolbarBtn}>
-        <IoBrushOutline size={20} />
-      </button>
+      <div className={styles.btnGroup}>
+        <button className={styles.toolbar__btn}>
+          <IoBrushOutline size={25} />
+        </button>
+        <button className={styles.toolbar__btn}>
+          <IoSquare size={25} />
+        </button>
+        <button className={styles.toolbar__btn}>
+          <IoEllipseOutline size={25} />
+        </button>
+        <button className={styles.toolbar__btn}>
+          <BsEraser size={25} />
+        </button>
+        <button className={styles.toolbar__btn}>
+          <BsSlashLg size={25} />
+        </button>
+        <input type="color" value="#0fcde6" />
+      </div>
+      <div className={styles.navigteGroup}>
+        <button className={styles.toolbar__btn}>
+          <IoArrowBack size={25} />
+        </button>
+        <button className={styles.toolbar__btn}>
+          <IoArrowForward size={25} />
+        </button>
+        <button className={styles.toolbar__btn}>
+          <IoSaveOutline size={25} />
+        </button>
+      </div>
     </div>
   );
 };
